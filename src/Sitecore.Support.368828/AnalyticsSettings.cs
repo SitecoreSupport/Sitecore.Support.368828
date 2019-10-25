2c01a7fdf275bbe41c72066cbbafb1ed2b0662b5
AleksandrChykun <aec@sitecore.net>
2019-10-25 15:34:29 +0300

    Commit patch

---
+++ src/Sitecore.Support.368828/AnalyticsSettings.cs
@@ -0,0 +1,126 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Web.IPAddresses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Xml;
+
+namespace Sitecore.Support.Analytics.Configuration
+{
+    public static class AnalyticsSettings
+    {
+        public static class Robots
+        {
+            private static ExcludeList excludeList;
+
+            private static object excludeListSync = new object();
+
+            public static bool AutoDetect => Settings.GetBoolSetting("Analytics.AutoDetectBots", defaultValue: true);
+
+            public static ExcludeList ExcludeList
+            {
+                get
+                {
+                    if (excludeList == null)
+                    {
+                        lock (excludeListSync)
+                        {
+                            if (excludeList == null)
+                            {
+                                excludeList = ExcludeList.GeExcludeList();
+                            }
+                        }
+                    }
+                    return excludeList;
+                }
+                internal set
+                {
+                    excludeList = value;
+                }
+            }
+
+            public static bool IgnoreRobots => Settings.GetBoolSetting("Analytics.Robots.IgnoreRobots", defaultValue: true);
+
+            public static int SessionTimeout => Settings.GetIntSetting("Analytics.Robots.SessionTimeout", 1);
+        }
+        public class ExcludeList
+        {
+            private HashSet<string> userAgents;
+
+            private IPList ipaddresses;
+
+            public ExcludeList(IPList ipaddresses, IEnumerable<string> userAgents)
+                : this(ipaddresses, new HashSet<string>(userAgents))
+            {
+            }
+
+            public ExcludeList(IPList ipaddresses, HashSet<string> userAgents)
+            {
+                this.ipaddresses = ipaddresses;
+                this.userAgents = userAgents;
+            }
+
+            public static ExcludeList GeExcludeList()
+            {
+                IPList iPAddresses = GetIPAddresses();
+                HashSet<string> hashSet = GetUserAgents();
+                return new ExcludeList(iPAddresses, hashSet);
+            }
+
+            public bool ContainsIpAddress(string addressString)
+            {
+                Assert.ArgumentNotNull(addressString, "addressString");
+                if (IPAddress.TryParse(addressString, out IPAddress address))
+                {
+                    return ipaddresses.Contains(address);
+                }
+                return false;
+            }
+
+            public bool ContainsUserAgent(string userAgent)
+            {
+                Assert.ArgumentNotNull(userAgent, "userAgent");
+
+                #region Sitecore.Support.368828
+
+                return userAgents.Contains(userAgent, System.StringComparer.OrdinalIgnoreCase);
+
+                #endregion
+
+            }
+
+            private static IPList GetIPAddresses()
+            {
+                XmlNode configNode = Factory.GetConfigNode("analyticsExcludeRobots/excludedIPAddresses");
+                if (configNode == null)
+                {
+                    return new IPList();
+                }
+                return IPHelper.GetIPList(configNode) ?? new IPList();
+            }
+
+            private static HashSet<string> GetUserAgents()
+            {
+                XmlNode configNode = Factory.GetConfigNode("analyticsExcludeRobots/excludedUserAgents");
+                HashSet<string> hashSet = new HashSet<string>();
+                if (configNode == null)
+                {
+                    return hashSet;
+                }
+                string[] array = configNode.InnerText.Split('\n');
+                for (int i = 0; i < array.Length; i++)
+                {
+                    string text = array[i].Trim();
+                    if (!(text == string.Empty) && !text.StartsWith("#") && !hashSet.Contains(text))
+                    {
+                        hashSet.Add(text);
+                    }
+                }
+                return hashSet;
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.368828/CheckUserAgent.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Analytics.Configuration;
+using Sitecore.Analytics.Pipelines.ExcludeRobots;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Sitecore.Support.Analytics.Pipelines.ExcludeRobots
+{
+    public class CheckUserAgent : Sitecore.Analytics.Pipelines.ExcludeRobots.CheckUserAgent
+    {
+        public override void Process(ExcludeRobotsArgs args)
+        {
+            var argsType = args.GetType();
+            PropertyInfo propInfo = argsType.GetProperty("HttpContext", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            HttpContextWrapper HttpContext = propInfo.GetValue(args, null) as System.Web.HttpContextWrapper;
+            
+            Assert.ArgumentNotNull(args, "args");
+            Assert.IsNotNull(HttpContext, "HttpContext");
+            Assert.IsNotNull(HttpContext.Request, "Request");
+            if (HttpContext.Request.UserAgent != null && Sitecore.Support.Analytics.Configuration.AnalyticsSettings.Robots.ExcludeList.ContainsUserAgent(HttpContext.Request.UserAgent))
+            {
+               args.IsInExcludeList = true;
+            }
+        }
+    }
+}
